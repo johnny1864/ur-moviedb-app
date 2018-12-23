cd616a38fd526cc9aae6aee473319ef38a41b0df
johnny1864 <johnny1864@yahoo.com>
2018-12-23 11:53:52 -0800

    fixed material module error

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from "@angular/common";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatCardModule],
-  exports: [MatToolbarModule, MatButtonModule, MatCardModule,, MatInputModule]
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
+  ],
+  exports: [MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule]
 })
 export class MaterialModule {}
